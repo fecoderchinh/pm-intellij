ad9b464a8d46b79e2dab4ed3956ab6db22588ec6
Fecoder <fecoder.chinh@gmail.com>
2021-11-18 15:40:04 +0700

    button image

--- src/main/java/fecoder/MainViewController.java
+++ src/main/java/fecoder/MainViewController.java
@@ -19,7 +19,7 @@ public class MainViewController {
     public Button packagingManage;
     public Button productManage;
 //    public Button packagingOwner;
-    public Button commands;
+    public Button workOrder;
     @FXML private Button logoutButton;
     @FXML private Label sessionLabel;
 
@@ -29,38 +29,54 @@ public class MainViewController {
 
     public void initSessionID(final LoginManager loginManager, String sessionID) {
         sessionLabel.setText(sessionID);
-        newWindowButton.setGraphic(new ImageView(new Image(getClass().getResource("/images/logo.jpg").toString())));
-        newWindowButton.setContentDisplay(ContentDisplay.TOP);
+
+//        newWindowButton.setGraphic(new ImageView(new Image(getClass().getResource("/images/logo.jpg").toString())));
+//        newWindowButton.setContentDisplay(ContentDisplay.TOP);
+
         logoutButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent event) {
                 loginManager.logout();
             }
         });
 
+        suplierManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/suppliers.png").toString(), 200, 200, true, true)));
+        suplierManage.setContentDisplay(ContentDisplay.TOP);
         suplierManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/suplier.fxml", "Quản Lý Danh Sách Nhà Cung Cấp");
         });
 
+        packagingTypeManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/package.png").toString(), 200, 200, true, true)));
+        packagingTypeManage.setContentDisplay(ContentDisplay.TOP);
         packagingTypeManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/type.fxml", "Quản Lý Loại Bao Bì");
         });
 
+        yearManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/calendar.png").toString(), 200, 200, true, true)));
+        yearManage.setContentDisplay(ContentDisplay.TOP);
         yearManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/year.fxml", "Quản Lý Loại Kích Thước");
         });
 
+        sizeManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/shrimp.png").toString(), 200, 200, true, true)));
+        sizeManage.setContentDisplay(ContentDisplay.TOP);
         sizeManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/size.fxml", "Quản lý size tôm");
         });
 
+        customerManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/customer.png").toString(), 200, 200, true, true)));
+        customerManage.setContentDisplay(ContentDisplay.TOP);
         customerManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/customer.fxml", "Quản lý khách hàng");
         });
 
+        packagingManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/packages.png").toString(), 200, 200, true, true)));
+        packagingManage.setContentDisplay(ContentDisplay.TOP);
         packagingManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/packaging.fxml", "Quản lý bao bì");
         });
 
+        productManage.setGraphic(new ImageView(new Image(getClass().getResource("/images/tempura.png").toString(), 200, 200, true, true)));
+        productManage.setContentDisplay(ContentDisplay.TOP);
         productManage.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/product.fxml", "Quản lý mặt hàng");
         });
@@ -69,7 +85,9 @@ public class MainViewController {
 //            utils.loadScene("/fxml/packaging_owner.fxml", "Quản lý bao bì trong mặt hàng");
 //        });
 
-        commands.setOnMouseClicked((event) -> {
+        workOrder.setGraphic(new ImageView(new Image(getClass().getResource("/images/note.png").toString(), 200, 200, true, true)));
+        workOrder.setContentDisplay(ContentDisplay.TOP);
+        workOrder.setOnMouseClicked((event) -> {
             utils.loadScene("/fxml/work_order.fxml", "Lệnh sản xuất");
         });
     }
